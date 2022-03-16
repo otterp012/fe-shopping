b389c8c4e293aa5e3bee183e9068082420055fa9
otterp012 <otterp012@gmail.com>
2022-03-16 11:30:51 +0900

    feat: express 서버

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
-const app = express();
 
-app.listen(5000);
+const app = express();
+const port = 3000;
+
+app.get('/', (request, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
