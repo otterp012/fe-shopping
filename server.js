e0eb75a54f3319d37942697fbc1608cfbccc7efd
otterp012 <otterp012@gmail.com>
2022-03-17 12:33:24 +0900

    flx: git충돌로 파일재업로드

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
+
+const categoryData = require('./data/data.json');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (request, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
+
+app.get('/categoryData', (req, res) => {
+  res.json(categoryData.filterOptions);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ src/js/app.js
@@ -0,0 +1,61 @@
+import { utils } from './utils.js';
+
+function selectBtnEventHandler() {
+  document.querySelector('.select-btn').addEventListener('click', () => {
+    document.querySelector('.select-options').classList.toggle('active');
+  });
+}
+
+async function renderFilterOptions() {
+  const filterOptionsJson = await utils.myFetch('/categoryData');
+  const filterOptions = filterOptionsJson.reduce((acc, str) => {
+    return acc + `<li class="select-option">${str}</li>`;
+  }, '');
+
+  document.querySelector('.select-options').innerHTML += filterOptions;
+}
+
+renderFilterOptions();
+selectBtnEventHandler();
+
+const autoSearchWordsEL = document.querySelector('.auto-search-words');
+// const timer = setTimeout(() => {
+//   if (document.querySelector('.search-bar').value) {
+//     if (!autoSearchWordsEL.classList.contains('active')) {
+//       autoSearchWordsEL.classList.add('active');
+//     }
+//   } else {
+//     if (autoSearchWordsEL.classList.contains('active'))
+//       autoSearchWordsEL.classList.remove('active');
+//   }
+// }, 500);
+
+if (document.querySelector('.search-bar').value) {
+  if (!autoSearchWordsEL.classList.contains('active')) {
+    autoSearchWordsEL.classList.add('active');
+  }
+} else {
+  if (autoSearchWordsEL.classList.contains('active'))
+    autoSearchWordsEL.classList.remove('active');
+}
+
+document.querySelector('.search-bar').addEventListener('keyup', () => {
+  renderSuggestions(document.querySelector('.search-bar').value);
+});
+
+function getSuggestions(prefix) {
+  return fetch(
+    `https://completion.amazon.com/api/2017/suggestions?session-id=133-4736477-7395454&customer-id=&request-id=4YM3EXKRH1QJB16MSJGT&page-type=Gateway&lop=en_US&site-variant=desktop&client-info=amazon-search-ui&mid=ATVPDKIKX0DER&alias=aps&b2b=0&fresh=0&ks=8&prefix=${prefix}&event=onKeyPress&limit=11&fb=1&suggestion-type=KEYWORD`
+  ).then((res) => res.json());
+}
+
+async function renderSuggestions(prefix) {
+  const suggestionsJson = await getSuggestions(prefix);
+  const suggestionsInfo = suggestionsJson.suggestions
+    .map((suggestion) => suggestion.value)
+    .reduce((acc, str) => {
+      return acc + `<li class="auto-search-word">${str}</li>`;
+    }, '');
+  document.querySelector('.auto-search-words').innerHTML = '';
+  document.querySelector('.auto-search-words').innerHTML += suggestionsInfo;
+}
