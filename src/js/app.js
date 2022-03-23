16f6bc5b67bc9632920c7e72031bd4f0f4f563ab
otterp012 <otterp012@gmail.com>
2022-03-23 16:30:25 +0900

    feat: 실행요소만 포함하게 수정, 카테고리 기능 일단삭제

--- src/js/app.js
+++ src/js/app.js
@@ -1,40 +1,21 @@
-import { SearchHistory, SearchAutoComplete } from './view/Searcher.js';
-import { selector, myFetch } from './utils/utils.js';
+import {
+  AutocompleteModel,
+  AutocompleteView,
+  AutocompleteCotroller,
+} from './search/autosearch.js';
+import {
+  SearchHistoryModel,
+  SearchHistoryView,
+  SearchHistoryController,
+} from './search/historysearch.js';
 
 function init() {
-  const searchHistory = new SearchHistory();
-  const searchAuto = new SearchAutoComplete();
+  new AutocompleteCotroller(new AutocompleteModel(), new AutocompleteView());
+
+  new SearchHistoryController(
+    new SearchHistoryModel(),
+    new SearchHistoryView()
+  );
 }
 
 init();
-
-// 아래 부분은 카테고리 이벤트 부분입니다.
-// 클래스로, 설계는 해놓았는데 아직 코드를 짜지 못했습니다. 🥲
-// 주말에 마무리 할 계획입니다!
-
-const selectOptionListsWrapperEl = document.querySelector(
-  '.select-option-lists'
-);
-async function renderFilterOptions() {
-  const filterOptionsJson = await myFetch('./categoryData');
-  const filterOptions = filterOptionsJson.reduce((acc, str) => {
-    return acc + `<li class="select-option-list">${str}</li>`;
-  }, '');
-
-  selectOptionListsWrapperEl.innerHTML += filterOptions;
-}
-renderFilterOptions();
-selectBtnClickEvent();
-
-function selectBtnClickEvent() {
-  document.querySelector('.select-btn').addEventListener('click', () => {
-    selectOptionListsWrapperEl.classList.toggle('active');
-  });
-}
-
-const selectBtnTextEl = document.querySelector('.select-btn-text');
-
-selectOptionListsWrapperEl.addEventListener('click', (e) => {
-  selectBtnTextEl.textContent = e.target.textContent;
-  selectOptionListsWrapperEl.classList.remove('active');
-});
