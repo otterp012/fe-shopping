ef6092aa927c5d65fd9c34c186e9fa754c1a27c0
otterp012 <otterp012@gmail.com>
2022-03-25 17:25:19 +0900

    fix: core 변경

--- src/js/core/core.js
+++ src/js/core/core.js
@@ -10,7 +10,9 @@ class Model {
     this.state = { ...this.state, ...newState };
   };
 
-  getState = (key) => this.state[key];
+  getState = (key) => {
+    return this.state[key];
+  };
 
   get = () => this.state;
 }
@@ -35,64 +37,15 @@ class Controller {
 
   setState = (newState) => {
     this.model.setState(newState);
-    this.render(this.model.getState(Object.keys(newState).join('')));
+  };
+
+  setStateProperty = (key, value) => {
+    this.model.state[key] = value;
   };
 
   render = (state) => {
-    this.view.render(state);
+    this.view.render(this.model.getState(state));
   };
 }
-
-const controllerUtils = {
-  hasValue: (node) => {
-    return !!node.value;
-  },
-
-  show: (node) => {
-    node.style.display = 'block';
-  },
-
-  hide: (node) => {
-    node.style.display = 'none';
-  },
-
-  isDisplayed: (node) => {
-    return node.style.display === 'none' ? false : true;
-  },
-
-  timer: (callback, ms) => {
-    setTimeout(() => callback(), ms);
-  },
-
-  addEvent: (node, type, eventHandler) => {
-    node.addEventListener(`${type}`, (event) => {
-      eventHandler(event);
-    });
-  },
-
-  addClassName: (node, className) => {
-    node.classList.add(className);
-  },
-
-  removeClassName: (node, className) => {
-    if (node.classList.contains(className)) {
-      node.classList.remove(className);
-    }
-  },
-
-  hasTargetParent: (node, target) => {
-    if (node) {
-      let current = node;
-      while (true) {
-        let parent = current.parentNode;
-        current = parent;
-        if (parent === document.body) return false;
-        if (parent === target) return true;
-      }
-    }
-  },
-};
-
-Object.assign(Controller.prototype, controllerUtils);
 
 export { Model, View, Controller };
